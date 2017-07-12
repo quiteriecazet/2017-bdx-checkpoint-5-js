bf24959bb6c684140669e8500fb3c8f638398506
Quiterie Cazet <q.cazet@gmail.com>
2017-07-12 12:21:53 +0200

    exercice 4 done

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,15 +1,23 @@
+var moment = require('moment');
+
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', '$interval', function (UsersService, $state, $interval) {
         'use strict'
         angular.extend(this, {
             $onInit() {
+
+                $interval(() => {
+                    this.date = moment().format('MMMM Do YYYY, h:mm: ss a')
+                }, 1000)
+                
                 UsersService.getCurrent().then((user) => {
                     this.user = user
                 }).catch((err) => {
 
-                })
+                });
             },
+            
             disconnect() {
                 UsersService.disconnect().then(() => {
                     Materialize.toast('Disconnected', 4000, 'toast-warning')
